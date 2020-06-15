29b8e803381bcf991a86f384b5dcc02af4bb48f4
ilhovSpace <ilgov.space@gmail.com>
2020-06-15 12:33:29 +0300

    object task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import './scss/index.scss'
+users = [{
+  id: 'as23',
+  nick: 'Octopus',
+  firstName: 'John',
+  LastName: 'Dou'
+}, {
+  id: 'as25',
+  nick: 'Star',
+  firstName: 'Andy',
+  LastName: 'Lee'
+}, {
+  id: 'as77',
+  nick: 'Wally',
+  firstName: 'Liza',
+  LastName: 'Corty'}]
 
-console.log('Working!')
+
+
+const object = {};
+users.forEach(({ id, ...rest }) => (object[id] = { ...rest }));
+
+console.log(object);
